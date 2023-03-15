866d1c84a28f18f34f9abad166147749ff64e03c
Guillaume Clement <guillaumecle.me@gmail.com>
2023-03-15 07:37:49 -0600

    Implement registration holder to keep track of registered services

--- core/src/main/java/me/guillaumecle/aem/soap/core/activators/CXFActivator.java
+++ core/src/main/java/me/guillaumecle/aem/soap/core/activators/CXFActivator.java
@@ -7,8 +7,6 @@ import java.util.List;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.InvalidSyntaxException;
-import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -22,8 +20,6 @@ import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import me.guillaumecle.aem.soap.core.endpoints.GreeterService;
-import me.guillaumecle.aem.soap.core.endpoints.GreeterServiceImpl;
 import me.guillaumecle.aem.soap.core.services.CXFService;
 import me.guillaumecle.aem.soap.core.services.CXFServiceInterface;
 
@@ -36,36 +32,35 @@ import me.guillaumecle.aem.soap.core.services.CXFServiceInterface;
 )
 @Designate(ocd = CXFActivator.ActivatorConfig.class)
 public class CXFActivator implements BundleActivator {
-    
-    @SuppressWarnings("rawtypes")
-    private static ServiceRegistration registration;
+
     private static BundleContext bundleContext;
     private static Dictionary<String, String> props;
 
     @Reference(	policy = ReferencePolicy.DYNAMIC, 
 				cardinality = ReferenceCardinality.MULTIPLE,
-				bind="bindModelList",
-				unbind="unbindModelList")
-	private volatile List<CXFService> cxfServices;
+				bind="bindService",
+				unbind="unbindService")
+	private volatile List<CXFService> cxfServices = new ArrayList<CXFService>();
+	private volatile List<RegistrationHolder> registrationHolders = new ArrayList<RegistrationHolder>();
 
     private Logger log = LoggerFactory.getLogger(getClass());
 
     @ObjectClassDefinition(name = "Apache CXF Activator Configuration", description = "Configures the Apache CXF SOAP Endpoints and Connector details. See https://cxf.apache.org/distributed-osgi-reference.html")
     public @interface ActivatorConfig {
 
-        @AttributeDefinition(name = "Denotes the interfaces to be exposed remotely. This is a comma-separated list of fully qualified Java interfaces that should be made available remotely. ")
+        @AttributeDefinition(name = "Interfaces to be exposed remotely", description = "This is a comma-separated list of fully qualified Java interfaces that should be made available remotely. Use * to list all registered interfaces.")
         String exportedInterfaces() default "*";
 
-        @AttributeDefinition(name = "Specifies the mechanism for configuring the service exposure.")
+        @AttributeDefinition(name = "Exported configurations", description = "Specifies the mechanism for configuring the service exposure.")
         String exportedConfigs() default "org.apache.cxf.ws";
 
-        @AttributeDefinition(name = "The address at which the service will be made available remotely.")
+        @AttributeDefinition(name = "CXF address", description = "The address at which the service will be made available remotely.")
         String address() default "http://0.0.0.0:4504/soap/endpoint";
 
-        @AttributeDefinition(name = "The CXF frontend which will be used to create endpoints.")
+        @AttributeDefinition(name = "CXF frontend", description = "The CXF frontend which will be used to create endpoints.")
         String frontend() default "simple";
 
-        @AttributeDefinition(name = "The CXF databindings which will be used to marshall objects")
+        @AttributeDefinition(name = "CXF databindings", description = "The CXF databindings which will be used to marshall objects")
         String databinding() default "aegis";
     }
 
@@ -78,49 +73,66 @@ public class CXFActivator implements BundleActivator {
     @Override
     public void stop(BundleContext context) throws Exception {
         log.info("Stopping CXF Bundle");
-
-        if(registration != null){
-            registration.unregister();
-        }
     }
 
     @Activate
     @Modified
     protected void activate(final ActivatorConfig config) throws Exception {
-        log.debug("Activating CXF Bundle with address [{}]", config.address());
+        log.info("Activating CXF Bundle with address [{}]", config.address());
         props = new Hashtable<String, String>();
 
         props.put("service.exported.interfaces", config.exportedInterfaces());
         props.put("service.exported.configs", config.exportedConfigs());
         props.put("org.apache.cxf.ws.address", config.address());
         props.put("org.apache.cxf.ws.frontend", config.frontend()); 
-        props.put("org.apache.cxf.ws.databinding", config.databinding());  
-
-        // for (ServiceReference<?> reference : references) {
-        //     log.debug("Registering service [{}]", reference.getClass().getName());
-        //     registration = context.registerService(CXFService.class.getName(), reference, props);
-        // }
-        //Potential to register interceptors via interface registration
-        // https://bitbucket.org/aftia/aftia-fbp/src/master/fbp/core/src/main/java/com/aftia/fbp/core/PojoWorkflowImpl.java
-
-        // ServiceReference<?>[] references = bundleContext.getAllServiceReferences(CXFService.class.getName(), null);
-
-        // log.info("Svcs:" + references.length);
+        props.put("org.apache.cxf.ws.databinding", config.databinding());
     }
 
-    protected synchronized void bindModelList(CXFService cxfService) {
-		if (null == cxfServices) {
-			cxfServices = new ArrayList<CXFService>();
-		}
-		
-		cxfServices.add(cxfService);
+    protected synchronized void bindService(CXFService cxfService) throws Exception {
+        CXFServiceInterface serviceInterface;
+        RegistrationHolder holder = new RegistrationHolder();
+        holder.setCxfService(cxfService);
 
-        registration = bundleContext.registerService(cxfService.getClass().getAnnotation(CXFServiceInterface.class).wsdlInterface().getName(), cxfService, props);
-		log.info("Bound service implementation: [" + cxfService.getClass().getName() + "].");
+        try {
+            serviceInterface = cxfService.getClass().getAnnotation(CXFServiceInterface.class);
+
+            ServiceRegistration<?> registration = bundleContext.registerService(serviceInterface.wsdlInterface().getName(), cxfService, props);
+            holder.setRegistration(registration);
+
+            registrationHolders.add(holder);
+
+            log.info("Bound service implementation: [" + cxfService.getClass().getName() + "].");
+        } catch (NullPointerException e) {
+            throw new Exception("Service registration is missing a valid @CXFServiceInterface annotation.", e);
+        }
 	}
 	
-	protected synchronized void unbindModelList(CXFService cxfService) {
-		cxfServices.remove(cxfService);
+	protected synchronized void unbindService(CXFService cxfService) {
+        registrationHolders.stream()
+            .filter(r -> r.getCxfService().equals(cxfService)).findFirst()
+            .ifPresent(r -> {
+                r.getRegistration().unregister();
+                registrationHolders.remove(r);
+            });
+
 		log.info("Unbound service implementation: [" + cxfService.getClass().getName() + "].");
 	}
+
+    class RegistrationHolder {
+        private CXFService cxfService;
+        private ServiceRegistration<?> registration;
+
+        public CXFService getCxfService() {
+            return cxfService;
+        }
+        public void setCxfService(CXFService cxfService) {
+            this.cxfService = cxfService;
+        }
+        public ServiceRegistration<?> getRegistration() {
+            return registration;
+        }
+        public void setRegistration(ServiceRegistration<?> registration) {
+            this.registration = registration;
+        }
+    }
 }
