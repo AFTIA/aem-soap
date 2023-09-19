24ec09f833d1554f12c0a1aebb5bb73712b705c5
Guillaume Clement <guillaumecle.me@gmail.com>
2023-09-19 08:25:31 -0600

    Updating annotations and props support to enable multiple endpoints

--- core/src/test/java/me/guillaumecle/aem/soap/core/annotations/ValidServiceImpl.java
+++ core/src/test/java/me/guillaumecle/aem/soap/core/annotations/ValidServiceImpl.java
@@ -1,6 +1,6 @@
 package me.guillaumecle.aem.soap.core.annotations;
 
-@CXFServiceInterface(wsdlInterface = ValidService.class)
+@CXFServiceInterface(wsdlInterface = ValidService.class, address = "")
 public class ValidServiceImpl implements ValidService{
     @Override
     public String getMessage() {
--- it.tests/src/main/java/me/guillaumecle/aem/soap/it/services/GreeterServiceImpl.java
+++ it.tests/src/main/java/me/guillaumecle/aem/soap/it/services/GreeterServiceImpl.java
@@ -11,7 +11,7 @@ import me.guillaumecle.aem.soap.core.services.CXFService;
 
 @WebService
 @Component(service = CXFService.class)
-@CXFServiceInterface(wsdlInterface = GreeterService.class)
+@CXFServiceInterface(wsdlInterface = GreeterService.class, address = "http://0.0.0.0:4504/soap/endpoint/greeter")
 public class GreeterServiceImpl implements CXFService, GreeterService {
 
     private Logger log = LoggerFactory.getLogger(getClass());
